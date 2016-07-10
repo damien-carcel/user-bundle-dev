e9cce6f3a6ec0e9ecbfbf08fd3a618bb8aeb40f8
Damien Carcel <damien.carcel@gmail.com>
2016-07-10 16:54:26 +0200

    Add phpspec unit tests

---
+++ spec/Form/Type/UserTypeSpec.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace spec\Carcel\Bundle\UserBundle\Form\Type;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class UserTypeSpec extends ObjectBehavior
+{
+    function let(TranslatorInterface $translator)
+    {
+        $this->beConstructedWith($translator, 'Carcel\Bundle\UserBundle\Entity\User');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Carcel\Bundle\UserBundle\Form\Type\UserType');
+    }
+
+    function it_is_a_form_type()
+    {
+        $this->shouldImplement('Symfony\Component\Form\FormTypeInterface');
+    }
+
+    function it_sets_a_default_configuration(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['data_class' => 'Carcel\Bundle\UserBundle\Entity\User'])->shouldBeCalled();
+
+        $this->configureOptions($resolver);
+    }
+
+    function it_builds_a_user_form($translator, FormBuilderInterface $builder)
+    {
+        $translator->trans('carcel_user.form.name.label')->willReturn('Name');
+        $translator->trans('carcel_user.form.email.label')->willReturn('Email');
+        $translator->trans('carcel_user.button.update')->willReturn('Update');
+
+        $builder
+            ->add('username', 'Symfony\Component\Form\Extension\Core\Type\TextType', ['label' => 'Name'])
+            ->shouldBeCalled();
+
+        $builder
+            ->add('email', 'Symfony\Component\Form\Extension\Core\Type\EmailType', ['label' => 'Email'])
+            ->shouldBeCalled();
+
+        $builder
+            ->add('submit', 'Symfony\Component\Form\Extension\Core\Type\SubmitType', ['label' => 'Update'])
+            ->shouldBeCalled();
+
+        $this->buildForm($builder, []);
+    }
+}
