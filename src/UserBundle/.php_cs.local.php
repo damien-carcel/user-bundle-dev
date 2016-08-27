96273fc5e77899f79c43accc13393b3a4d9971b6
Damien Carcel <damien.carcel@gmail.com>
2016-08-27 14:38:09 +0200

    Prepare UserBundle for repository split

--- src/UserBundle/.php_cs.local.php
+++ src/UserBundle/.php_cs.local.php
@@ -4,7 +4,6 @@ $finder = \Symfony\CS\Finder\DefaultFinder::create()->files();
 $fixers = require __DIR__.'/.php_cs-fixers.php';
 
 $finder->name('*.php')
-    ->in(__DIR__.'/features')
     ->in(__DIR__.'/src')
     ->notName('*Spec.php');
 
